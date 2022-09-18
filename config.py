ee27397d94c025c6e2cdd5f0ec39ed01e9c1f0bf
JingShiong <jingshiong@gmail.com>
2022-09-18 18:11:33 +0800

    s

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ccgjgmffszz7.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "liawjingshiong-employee"
+customregion = "us-east-1"
